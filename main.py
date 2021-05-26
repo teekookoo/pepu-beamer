2e6c8f8e2441e61cdcf35c4f4830dc828618f372
Tarmo Kivioja <tarmo.kivioja@gmail.com>
2021-05-26 20:19:32 +0300

    Added comments

--- main.py
+++ main.py
@@ -5,35 +5,65 @@ import random
 import os
 
 
+# Adjust these to customize the program
+# TODO implement as command line flags
+
+# Author to show on title slide
 AUTHOR_NAME = 'Tarmo Kivioja'
-RAFFLE_PREFIX = ''
-RAFFLE_POSTFIX = ' putosi :('
+
+# Title of intro slide
 INTRO_TITLE = 'Osallistujat'
+
+# The format of raffling slide titles is
+# [RAFFLE_PREFIX][ELIMINATED_NAME][RAFFLE_SUFFIX]
+RAFFLE_PREFIX = ''
+RAFFLE_SUFFIX = ' putosi :('
+
+# Title of the last slide with winner name
 CONGRATS_TITLE = 'Onnea voittajalle!'
+
+# Name of the file that contains participants, one per row
 PARTICIPANTS_FILE = 'osallistujat.txt'
+
+# Name of the .tex template from which the slides are generated
+# Should contain the following strings somewhere
+#   %% AUTHOR_NAME %%
+#   %% INTRO_FRAME %%
+#   %% RAFFLE_FRAMES %%
+#   %% CONGRATS_FRAME %%
 TEMPLATE_FILE = 'rigged.template.tex'
+
+# Name of the intermediate .tex file,
+# The end result will have the same name with .pdf suffix
 TEX_FILE = 'rigged.tex'
+
+# Should the program compile and open the resulting slides with latexmk?
+COMPILE_AND_PREVIEW = True
 
 
 def main():
-    template = read_template()
-    participants = read_participants()
+    template = read_template(TEMPLATE_FILE)
+    participants = read_participants(PARTICIPANTS_FILE)
     tex_contents = make_tex(template, participants)
     write_tex(tex_contents)
-    os.system(f'latexmk -pdf -pv {TEX_FILE}')
+    if COMPILE_AND_PREVIEW:
+      os.system(f'latexmk -pdf -pv {TEX_FILE}')
     return 0
 
 
-def read_template():
-    with open(TEMPLATE_FILE) as f:
+# Read the template file and return as a string
+def read_template(filename):
+    with open(filename) as f:
         return f.read()
 
 
-def read_participants():
-    with open(PARTICIPANTS_FILE) as f:
+# Read the participant file and return as a list of strings
+def read_participants(filename):
+    with open(filename) as f:
         return [name.strip() for name in f]
 
 
+# Turn the template string into a valid .tex string
 def make_tex(template, participants):
     raffle_frames, winner = make_raffle(participants)
     return (template
@@ -44,6 +74,8 @@ def make_tex(template, participants):
             )
 
 
+# Wrap `contents` in \begin{frame} ... \end{frame},
+# optionally setting `title`
 def make_frame(contents, title=''):
     if title == '':
       return '\n'.join([
@@ -59,6 +91,10 @@ def make_frame(contents, title=''):
           r'\end{frame}'
       ])
 
+# Turn the items into a multicolumn itmeize list
+# `items` is a list of (string, bool) tuples where
+# the string is the participant name and
+# the bool indicates whether the participant has been eliminated
 def make_list(items):
     def color(name, eliminated):
         return f'\\textcolor{{elim}}{{{name}}}' if eliminated else f'\\textbf{{{name}}}'
@@ -70,11 +106,14 @@ def make_list(items):
                       r'\end{AutoMultiColItemize}'])
 
 
+# Generate the slide that lists all participants before raffling
 def make_intro(participants):
     items = [(p, False) for p in participants]
     return make_frame(make_list(items), INTRO_TITLE)
 
 
+# Generate the raffling slides where one participant is eliminiated
+# per slide
 def make_raffle(participants):
     elimination_order = [name for name in participants]
     random.shuffle(elimination_order)
@@ -83,11 +122,12 @@ def make_raffle(participants):
     for eliminate in elimination_order[:-1]:
         eliminated.add(eliminate)
         items = [(name, name in eliminated) for name in participants]
-        title = f'{RAFFLE_PREFIX}{eliminate}{RAFFLE_POSTFIX}'
+        title = f'{RAFFLE_PREFIX}{eliminate}{RAFFLE_SUFFIX}'
         frames.append(make_frame(make_list(items), title))
     return ('\n\n'.join(frames), elimination_order[-1])
 
 
+# Generate the last slide that contains the winner
 def make_congrats(winner):
     contents = '\n'.join([
         r'\begin{center}',
@@ -97,10 +137,12 @@ def make_congrats(winner):
     return make_frame(contents, CONGRATS_TITLE)
 
 
+# Save `contents` to `TEX_FILE`
 def write_tex(contents):
     with open(TEX_FILE, 'w') as f:
         f.write(contents)
     
 
+# `main` is the entry point
 if __name__ == '__main__':
     main()
